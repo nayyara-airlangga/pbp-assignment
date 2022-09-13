2b88365bcf2a7cf992c40496eb274b52524276c0
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-09-13 15:36:30 +0700

    Create catalog view

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    catalog_items = CatalogItem.objects.all()
+
+    context = {
+        'name': 'Nayyara Airlangga Raharjo',
+        'student_id': '2106652070',
+        'catalog_items': catalog_items,
+    }
+
+    return render(request, 'katalog.html', context)

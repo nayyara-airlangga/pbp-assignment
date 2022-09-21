1b1c89c9ceafd296dbf6834209db5cfd8983741b
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-21 21:32:45 +0700

    Add mywatchlist html view

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import mywatchlist_html, mywatchlist
 
 app_name = 'mywatchlist'
-urlpatterns = [path('', show_mywatchlist, name='show_mywatchlist')]
+urlpatterns = [
+    path('', mywatchlist, name='mywatchlist'),
+    path('html/', mywatchlist_html, name='mywatchlist_html'),
+]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from mywatchlist.models import MyWatchlist
+
 # Create your views here.
-def show_mywatchlist(request):
+def mywatchlist(request):
     return render(request, 'My Watchlist')
+
+
+def mywatchlist_html(request):
+    mywatchlist_items = MyWatchlist.objects.all()
+
+    return render(
+        request,
+        'mywatchlist.html',
+        {'name': 'Angga', 'mywatchlist_items': mywatchlist_items},
+    )

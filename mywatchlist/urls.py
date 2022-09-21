069e66702a6686dc9424637b9230dd4daa0e7fc9
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-21 21:42:29 +0700

    Add mywatchlist xml and json view

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from mywatchlist.views import mywatchlist_html, mywatchlist
+from mywatchlist.views import (
+    mywatchlist_html,
+    mywatchlist,
+    mywatchlist_json,
+    mywatchlist_xml,
+)
 
 app_name = 'mywatchlist'
 urlpatterns = [
     path('', mywatchlist, name='mywatchlist'),
     path('html/', mywatchlist_html, name='mywatchlist_html'),
+    path('xml/', mywatchlist_xml, name='mywatchlist_xml'),
+    path('json/', mywatchlist_json, name='mywatchlist_json'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.shortcuts import HttpResponse, render
 
 from mywatchlist.models import MyWatchlist
 
 # Create your views here.
 def mywatchlist(request):
-    return render(request, 'My Watchlist')
+    mywatchlist_items = MyWatchlist.objects.all()
+
+    return render(
+        request,
+        'mywatchlist.html',
+        {'name': 'Angga', 'mywatchlist_items': mywatchlist_items},
+    )
 
 
 def mywatchlist_html(request):
@@ -14,4 +21,22 @@ def mywatchlist_html(request):
         request,
         'mywatchlist.html',
         {'name': 'Angga', 'mywatchlist_items': mywatchlist_items},
+    )
+
+
+def mywatchlist_xml(request):
+    mywatchlist_items = MyWatchlist.objects.all()
+
+    return HttpResponse(
+        serializers.serialize('xml', mywatchlist_items),
+        content_type='application/xml',
+    )
+
+
+def mywatchlist_json(request):
+    mywatchlist_items = MyWatchlist.objects.all()
+
+    return HttpResponse(
+        serializers.serialize('json', mywatchlist_items),
+        content_type='application/json',
     )

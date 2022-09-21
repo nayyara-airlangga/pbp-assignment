ee6f0d5610e53d1a2c0f9b72b248367020d5200e
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-21 14:03:19 +0700

    Make MyWatchlist model

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class MyWatchlistRating(models.IntegerChoices):
+    BAD = 1
+    NOT_GOOD = 2
+    OK = 3
+    GOOD = 4
+    GREAT = 5
+
+
+class MyWatchlist(models.Model):
+    watched = models.BooleanField()
+    title = models.CharField(max_length=255)
+    rating = models.IntegerField(choices=MyWatchlistRating.choices)
+    release_date = models.DateField()
+    review = models.TextField()

10ae26c9eb873e213d562d2eb3a8a21f1b4755dd
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-21 21:50:07 +0700

    Add mywatchlist views unit tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class MyWatchlistTestcase(TestCase):
+    def setUp(self):
+        self.__client = Client()
+
+    def test_html_view_ok(self):
+        url = reverse('mywatchlist:mywatchlist_html')
+        res = self.__client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_xml_view_ok(self):
+        url = reverse('mywatchlist:mywatchlist_xml')
+        res = self.__client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_json_view_ok(self):
+        url = reverse('mywatchlist:mywatchlist_json')
+        res = self.__client.get(url)
+
+        self.assertEqual(res.status_code, 200)

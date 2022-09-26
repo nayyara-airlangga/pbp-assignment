41e75e409a6c819fb50cf31777eb1dee904dd847
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-26 21:56:22 +0700

    Add default now for Task date

--- todolist/models.py
+++ todolist/models.py
@@ -1,9 +1,10 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     title = models.CharField(max_length=255)
     description = models.TextField()
